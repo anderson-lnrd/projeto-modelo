ca34ae95d1bb03348692356a88f3d21397091a04
Anderson Leonardo <81546927+anderson-lnrd@users.noreply.github.com>
2023-04-27 13:35:12 -0300

    ommit inicial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'prjeto-modelo'`, () => {
+  it(`should have as title 'projeto-modelo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('prjeto-modelo');
+    expect(app.title).toEqual('projeto-modelo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('prjeto-modelo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('projeto-modelo app is running!');
   });
 });
